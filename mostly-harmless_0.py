f636f0ca3302a68207cb7e7dba1f82d866363e01
Timothy Pavlushik <49930425+LemonPi314@users.noreply.github.com>
2021-04-02 01:19:43 -0700

    Added f-strings, updated readme.md

--- mostly-harmless_0.py
+++ mostly-harmless_0.py
@@ -1,17 +1,16 @@
 # 0
 import os, sys, subprocess, time, pyautogui
-number = __file__.split("_", 1)[1]
+number = __file__.split('_', 1)[1]
 number = int(number[:len(number)-3]) + 1
-filePath = str(os.getcwd() + "\\mostly-harmless_%s.py" % number)
-file = open(filePath, "w")
-file.close()
-subprocess.Popen(["notepad.exe", filePath])
+filePath = f'{os.getcwd()}\\mostly-harmless_{number}.py'
+file = open(filePath, 'w').close()
+subprocess.Popen(['notepad.exe', filePath])
 time.sleep(1)
-with open(__file__, "r") as currentFile:
+with open(__file__, 'r') as currentFile:
     lines = currentFile.readlines()[1:]
-    pyautogui.typewrite("# %s\n" % number, interval=0.05)
+    pyautogui.typewrite(f'# {number}\n', interval=0.05)
     for line in lines:
         pyautogui.typewrite(line, interval=0.05)
-    pyautogui.hotkey("ctrl", "s")
-    pyautogui.hotkey("alt", "f4")
-subprocess.Popen(["start", sys.executable, filePath], shell=True)
+    pyautogui.hotkey('ctrl', 's')
+    pyautogui.hotkey('alt', 'f4')
+subprocess.Popen(['start', sys.executable, filePath], shell=True)
--- readme.md
+++ readme.md
@@ -1,10 +1,11 @@
-# mostly-harmless
+# Mostly Harmless
 By [LemonPi314](https://github.com/LemonPi314)
 
 Just another tiny program that's, well, mostly harmless.  
 (And yes, the title is a reference to the Douglas Adams novel series "The Hitchhiker's Guide to the Galaxy")
 ## Requirements
 * Windows operating system
+* [Python 3.9](https://www.python.org/downloads/) or higher
 * [pyautogui](https://pypi.org/project/PyAutoGUI/)
 ## License
 [MIT License](https://choosealicense.com/licenses/mit/)
